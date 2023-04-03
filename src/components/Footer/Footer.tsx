d210751bb0f0b6419514ab51f7371577890f09dd
Yuriy Stepaniuk <yur.stepanyuk@gmail.com>
2023-04-03 18:12:30 +0300

    add empty components

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+type FooterProps = {};
+
+const Footer = (props: FooterProps) => {
+  return <div>Footer</div>;
+};
---
+++ src/components/NavBar/NavBar.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+type NavBarProps = {};
+
+const NavBar = (props: NavBarProps) => {
+  return <div>NavBar</div>;
+};
