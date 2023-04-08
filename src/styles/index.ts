11cf758be5198eeca7b676d832a883c772471e7c
Екатерина Кукурузова <kukuruzova.ekaterina@gmail.com>
2023-04-08 21:38:15 +0300

    Create theme

---
+++ src/styles/index.ts
@@ -0,0 +1,14 @@
+import createTheme from "@mui/material/styles/createTheme";
+
+const overrides = {
+    palette: {
+    },
+    typography: {
+    },
+    components: {
+    }
+};
+
+const theme = createTheme();
+
+export default theme;
